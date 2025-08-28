f59be597d0ab4b3ee12261ac4e975ed32a58711b
Robert Jensen <jensen.robert@gmail.com>
2025-08-28 10:28:22 +0200

    Add a data saver to the measurement script

--- LED/led_measure.py
+++ LED/led_measure.py
@@ -1,4 +1,6 @@
+import csv
 import time
+import datetime
 
 import pyvisa
 import nidaqmx
@@ -16,7 +18,7 @@ class PowerSupply:
         self.comm.baud_rate = 2400
         self.comm.stop_bits = pyvisa.constants.StopBits.one
         self.comm.write_termination = '\r'
-        self.max_voltage = 3
+        self.max_voltage = 5
         self.voltage_setpoint = None  # Will be set to in next line
         self.set_voltage(0)
 
@@ -68,11 +70,34 @@ class PowerSupply:
         self.comm.write(cmd)
 
 
+class DataWriter:
+    """
+    This class provides a way to store two data-files with the same data. One
+    file is named with a unique name that ensure that no data is lost. The
+    other file is always called `pid_plot.csv` and will be used by the plotting
+    programm.
+    """
+
+    def __init__(self):
+        now = datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+        filename = 'data_' + now + '.csv'
+        self.liveplot = open('led_plot.csv', 'w', newline='\n')
+        self.datafile = open(filename, 'w', newline='\n')
+        self.livewriter = csv.writer(self.liveplot, delimiter=';')
+        self.datawriter = csv.writer(self.datafile, delimiter=';')
+
+    def write_line(self, **kwargs):
+        self.livewriter.writerow(kwargs.values())
+        self.datawriter.writerow(kwargs.values())
+        self.liveplot.flush()
+        self.datafile.flush()
+
+
 class DataReader:
     def __init__(self):
         self.shunt = 100  # ohm
         self.sample_rate = 1000
-        self.samples = 200
+        self.samples = 250
 
     def _read_channel_voltage(self, channel):
         """
@@ -125,10 +150,12 @@ class LEDSweeper:
         self.ps = PowerSupply()
         self.ps.set_voltage(0)
         self.reader = DataReader()
+        self.writer = DataWriter()
         time.sleep(0.2)
         # Measure the offset at zero - this is typically
         # not very large and could be omitted
         self.i_0 = self.reader.read_current()
+        self.t_start = time.time()
 
     def sweep(self, max_current=10):
         """
@@ -138,17 +165,25 @@ class LEDSweeper:
         reached.
         """
         current = 0
-        voltage = 0
+        voltage = 1  # No usable data below 1V
         while current < max_current:
-            current = self.reader.read_current() - self.i_0
-            led_voltage = self.reader.read_voltage()
             voltage += 0.01
             self.ps.set_voltage(voltage)
+            time.sleep(0.1)
+            dt = time.time() - self.t_start
+            current = self.reader.read_current() - self.i_0
+            led_voltage = self.reader.read_voltage()
             msg = 'PS: {:.3f}V, I={:.3f}mA, V_LED={:.3f}V'
             print(msg.format(voltage, current, led_voltage))
+            self.writer.write_line(
+                time=dt,
+                voltage_setpoint=voltage,
+                v_led=led_voltage,
+                current=current,
+            )
         self.ps.set_voltage(0)
 
 
 if __name__ == '__main__':
     sweep = LEDSweeper()
-    sweep.sweep(3)
+    sweep.sweep(1)
